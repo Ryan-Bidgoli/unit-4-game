22f5fb7dd7e22bdfe107227958b98310a853bc74
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-10-30 21:26:11 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,5 +8,11 @@ $(document).ready(function() {
 var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
 $("#randomNumber").html(computerGuess);
 
+ 
+var stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+
 
 });
