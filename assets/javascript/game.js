eeb0acf156f2bda6b6c95ef0576e8ce7759c4bb8
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-10-30 21:52:16 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,23 +5,26 @@ $(document).ready(function() {
   var lost = 0;
   var score = 0;
 
+// Create a computer generator number for match
 var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
 $("#randomNumber").html(computerGuess);
 
- 
+//converting the string in integer
 var stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
 
- 
+//testing all random number
 console.log("computerGuess" + computerGuess);
 console.log("stone1 " + stone1);
 console.log("stone2 " + stone2);
 console.log("stone3 " + stone3);
 console.log("stone4 " + stone4);
 
- function reset () {
+//creating reset function the game
+
+function reset () {
   
   score = 0;
   $("#score").html(score);
@@ -33,7 +36,11 @@ console.log("stone4 " + stone4);
   stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
   stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
   stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
-  }
+
+
+}
+
+//create a click function for images generate number
   
 
 $("#stone1").on("click", function() {
@@ -46,10 +53,8 @@ $("#stone1").on("click", function() {
           }else if (score > computerGuess) {
               checkifwon();
           }
-
 });
-
-   
+  
 $("#stone2").on("click", function() {
   
           score = score + stone2;
@@ -61,7 +66,7 @@ $("#stone2").on("click", function() {
               checkifwon();
           }
 });
-   
+  
 $("#stone3").on("click", function() {
   
           score = score + stone3;
@@ -86,4 +91,25 @@ $("#stone4").on("click", function() {
           }
 });
 
+   
+function checkifwon() {
+  
+  if(score === computerGuess) {
+  
+  wins++;
+  console.log(wins);
+  $("#wins").html("You Win");
+  $("#win").html("win: " +wins);
+  reset();
+} else if (score > computerGuess) {
+  
+  lost++;
+  console.log(lost);
+  $("#wins").html("You Lost");
+  $("#lost").html("Lost: " + lost);
+  reset();
+}
+
+}
+  
 });
