7c0b7a97738067193d8607e1c2bdb797a7b8b546
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-10-30 21:16:37 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,12 @@
-var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var wins = 0;
-var losses = 0;
-var guessesLeft = 10; 
-var lettersGuessed = [];
-var playerGuess = document.onkeyup;
-var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
-document.onkeyup = function (event) {
-  
-  playerGuess = event.key;
- if(lettersGuessed.includes(playerGuess)) {
-    return;
-  }
-   
-  lettersGuessed.push(playerGuess);
-  if (computerGuess == playerGuess) {
-    wins++;
-    guessesLeft = 10;
-    guessesMade = [];
-  }
-  
-  if (computerGuess != playerGuess) {
-    guessesLeft--;
-    
-  }
+$(document).ready(function() {
 
-  if (guessesLeft == 0) {
-    losses++;
-    guessesLeft = 10;
-    guessesMade = [];
-  }
+  var wins = 0;
+  var lost = 0;
+  var score = 0;
 
-  var html =
-    '<p>Guess what letter I am thinking of</p>' +
-    '<p>Wins: ' + wins + '</p>' +
-    '<p>Losses: ' + losses + '</p>' +
-    '<p>Guesses Left: ' + guessesLeft + '</p>' +
-    '<p>Your Guesses: ' + lettersGuessed + '</p>';
-document.getElementById("game").innerHTML = html;
-}
+var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+$("#randomNumber").html(computerGuess);
+
+
+});
